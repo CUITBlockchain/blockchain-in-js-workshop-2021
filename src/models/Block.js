92ff29acc61cfbe602817a85cfa32086867d7cbe
Ranger <43808588+RangerX42@users.noreply.github.com>
2023-04-30 16:48:05 +0800

    update file

--- src/models/Block.js
+++ src/models/Block.js
@@ -2,33 +2,35 @@ import sha256 from 'crypto-js/sha256.js'
 
 export const DIFFICULTY = 2
 
-class Block {
-  // 1. 完成构造函数及其参数
+// class Block {
+//   // 1. 完成构造函数及其参数
 
-  constructor() {}
+//   constructor() {}
 
-  isValid() {}
+//   isValid() {}
 
-  setNonce(nonce) {}
+//   setNonce(nonce) {}
 
-  // 根据交易变化更新区块 hash
-  _setHash() {}
+//   // 根据交易变化更新区块 hash
+//   _setHash() {}
 
-  // 汇总计算交易的 Hash 值
-  /**
-   * 默克尔树实现
-   */
-  combinedTransactionsHash() {
+//   // 汇总计算交易的 Hash 值
+//   /**
+//    * 默克尔树实现
+//    */
+//   combinedTransactionsHash() {
 
-  }
+//   }
 
-  // 添加交易到区块
-  /**
-   * 
-   * 需包含 UTXOPool 的更新与 hash 的更新
-   */
-  addTransaction() {}
+//   // 添加交易到区块
+//   /**
+//    * 
+//    * 需包含 UTXOPool 的更新与 hash 的更新
+//    */
+//   addTransaction() {}
 
-}
+// }
 
-export default Block
+// export default Block
+
+
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,99 +1,43 @@
 import UTXOPool from './UTXOPool.js'
 
-// Blockchain
-// class Blockchain {
-//   // 1. 完成构造函数及其参数
-//   /* 构造函数需要包含
-//       - 名字
-//       - 创世区块
-//       - 存储区块的映射
-//   */
-//   constructor() {}
-
-//   // 2. 定义 longestChain 函数
-//   /*
-//     返回当前链中最长的区块信息列表
-//   */
-//   longestChain() {
-//     return []
-//   }
-
-//   // 判断当前区块链是否包含
-//   containsBlock(block) {
-//     // 添加判断方法
-//     return false
-//   }
-
-//   // 获得区块高度最高的区块
-//   maxHeightBlock() {
-//     // return Block
-//   }
-
-//   // 添加区块
-//   /*
-
-//   */
-//   _addBlock(block) {
-//     if (!block.isValid()) return
-//     if (this.containsBlock(block)) return
-//   }
-// }
-
-// export default Blockchain
-
-// ===
-
-import { maxBy, reduce, unfold, reverse, values, prop } from 'ramda'
-
-// Blockchain
+Blockchain
 class Blockchain {
-  // 构造函数
-  constructor(name) {
-    this.name = name
-    this.genesis = null
-    this.blocks = {}
+  // 1. 完成构造函数及其参数
+  /* 构造函数需要包含
+      - 名字
+      - 创世区块
+      - 存储区块的映射
+  */
+  constructor() {}
 
-    this.pendingTransactions = {}
-  }
-
+  // 2. 定义 longestChain 函数
+  /*
+    返回当前链中最长的区块信息列表
+  */
   longestChain() {
-    const blocks = values(this.blocks)
-    const maxByHeight = maxBy(prop('height'))
-    const maxHeightBlock = reduce(maxByHeight, blocks[0], blocks)
-    const getParent = (x) => {
-      if (x === undefined) {
-        return false
-      }
-
-      return [x, this.blocks[x.parentHash]]
-    }
-    return reverse(unfold(getParent, maxHeightBlock))
+    return []
   }
 
+  // 判断当前区块链是否包含
   containsBlock(block) {
-    return this.blocks[block.hash] !== undefined
+    // 添加判断方法
+    return false
   }
 
+  // 获得区块高度最高的区块
+  maxHeightBlock() {
+    // return Block
+  }
+
+  // 添加区块
+  /*
+
+  */
   _addBlock(block) {
     if (!block.isValid()) return
     if (this.containsBlock(block)) return
 
-    // check that the parent is actually existent and the advertised height is correct
-    const parent = this.blocks[block.parentHash]
-    if (
-      block.height != 1 &&
-      parent === undefined &&
-      parent.height + 1 !== block.height
-    )
-      return
-
-    // Add coinbase coin to the pool of the parent
-    const newUtxoPool =
-      parent && parent.utxoPool ? parent.utxoPool.clone() : new UTXOPool()
-    newUtxoPool.addUTXO(block.coinbaseBeneficiary, 12.5)
-    block.utxoPool = newUtxoPool
-
-    this.blocks[block.hash] = block
+    // 添加 UTXO 快照与更新的相关逻辑
   }
 }
 
