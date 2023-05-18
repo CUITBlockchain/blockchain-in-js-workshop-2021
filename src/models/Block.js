01ac89d3e11d55f21301d0ab1f28a35eac09af54
MAarcus5 <754846565@qq.com>
2023-05-18 14:31:23 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -2,36 +2,42 @@ import sha256 from 'crypto-js/sha256.js'
 
 export const DIFFICULTY = 2
 
+// Block 类表示区块
 class Block {
   constructor(blockchain, prevHash, height, data) {
-    this.blockchain = blockchain
-    this.prevHash = prevHash
-    this.height = height
-    this.data = data
-    this.nonce = -1
-    this.hash = ''
+    this.blockchain = blockchain  // 所属的区块链对象
+    this.prevHash = prevHash  // 前一个区块的哈希值
+    this.height = height  // 区块的高度
+    this.data = data  // 区块存储的数据
+    this.nonce = -1  // 随机数（用于工作量证明）
+    this.hash = ''  // 区块的哈希值
   }
 
+  // 判断区块是否有效（满足区块难度要求）
+  /*isValid：判断区块是否有效，即区块的哈希值是否满足区块难度要求*/
   isValid() {
-    const prefix = '0'.repeat(DIFFICULTY)
-    return this.hash.startsWith(prefix)
+    const prefix = '0'.repeat(DIFFICULTY)  // 区块哈希值的前缀要求（由难度决定）
+    return this.hash.startsWith(prefix)  // 判断区块的哈希值是否以指定前缀开头
   }
 
+  // 设置区块的随机数（计算并更新区块的哈希值）
   setNonce(nonce) {
-    this.nonce = nonce
-    this.hash = this.computeHash()
+    this.nonce = nonce  // 设置随机数
+    this.hash = this.computeHash()  // 计算并更新区块的哈希值
   }
 
+  // 计算区块的哈希值
   computeHash() {
     return sha256(
-      this.blockchain.name +
-        this.prevHash +
-        this.height.toString() +
-        this.data +
-        this.nonce.toString()
-    ).toString()
+      this.blockchain.name +  // 区块链的名称
+        this.prevHash +  // 前一个区块的哈希值
+        this.height.toString() +  // 区块的高度（转换为字符串）
+        this.data +  // 区块存储的数据
+        this.nonce.toString()  // 随机数（转换为字符串）
+    ).toString()  // 返回计算得到的哈希值
   }
 }
 
 export default Block
+
 
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -5,30 +5,40 @@ class Blockchain {
       - 名字
       - 创世区块
       - 存储区块的映射
+      constructor：构造函数，接收一个参数 name 表示区块链的名称。
+      初始化 name、genesis（初始值为 null，表示创世区块）和 blocks（用于存储所有区块的对象）
   */
       constructor(name) {
-        this.name = name;
-        this.genesis = null;
-        this.blocks = {};
+        this.name = name  // 区块链的名称
+        this.genesis = null  // 创世区块的哈希值
+        this.blocks = {}  // 存储区块链中的所有区块
       }
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
+    longestChain：查找最长的区块链。
+    首先初始化 maxHeight 为 -1（初始值为最小高度）和 maxHeightBlock 为 null。
+    然后遍历所有区块，找到高度最大的区块，并将其赋值给 maxHeightBlock。
+    接下来，创建一个空数组 chain 用于存储最长的区块链。
+    然后从最高区块开始，逐个向前查找前一个区块，将当前区块添加到 chain 数组的开头，直到达到创世区块。
+    最后返回 chain 数组作为最长的区块链
   */
     longestChain() {
-      let maxHeight = -1
-      let maxHeightBlock = null
+      let MaxHeight = -1  // 最大高度初始值为 -1
+      let MaxHeightBlock = null  // 最大高度的区块初始值为 null
   
+      // 遍历所有区块，找到最大高度的区块
       for (const block of Object.values(this.blocks)) {
-        if (block.height > maxHeight) {
-          maxHeight = block.height
-          maxHeightBlock = block
+        if (block.height > MaxHeight) {
+          MaxHeight = block.height
+          MaxHeightBlock = block
         }
       }
   
-      const chain = []
-      let currentBlock = maxHeightBlock
+      const chain = []  // 存储最长链上的所有区块
+      let currentBlock = MaxHeightBlock
   
+      // 从最大高度的区块开始，依次向前遍历区块链，构建最长链
       while (currentBlock) {
         chain.unshift(currentBlock)
         currentBlock = this.blocks[currentBlock.prevHash]
